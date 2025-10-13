a74ac741c4b6619973df51d9ce0055296ee97f9a
Dilujan-Uki <dilujan.uki.stu@dreamspace.academy>
2025-10-13 09:18:15 +0530

    day2

---
+++ day2/src/components/Day2.jsx
@@ -0,0 +1,7 @@
+export default function Day2(){
+    return (
+        <>
+        <h1 style={{color:'blue',background:'red',display:'flex',justifyContent:'center'}}>Hello!</h1>
+        </>
+    )
+}
