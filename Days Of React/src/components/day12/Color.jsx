94d1170ed11b63589f2930a68b4f5cd1172b35fd
Dilujan-Uki <dilujan.uki.stu@dreamspace.academy>
2025-12-09 15:40:55 +0530

    Perfect box shadow as well

--- Days Of React/src/components/day12/Color.jsx
+++ Days Of React/src/components/day12/Color.jsx
@@ -1,29 +1,34 @@
 import { useState } from "react"
-export default function Color(){
-      const [boxColor, setBoxColor] = useState("white");
-   
-    return (
-        <>
-        <div style={{ padding: "20px" }}>
-     
-      <div style={{ marginBottom: "20px", display: "flex", gap: "10px" }}>
-        <button className="px-4 py-2 border border-green-500 text-red-500 rounded-md hover:bg-red-500 hover:text-white" onClick={() => setBoxColor("red")}>Red</button>
-        <button className="px-4 py-2 border border-green-500 text-blue-500 rounded-md hover:bg-blue-500 hover:text-white" onClick={() => setBoxColor("blue")}>Blue</button>
-        <button className="px-4 py-2 border border-green-500 text-green-500 rounded-md hover:bg-green-500 hover:text-white" onClick={() => setBoxColor("green")}>Green</button>
-        <button className="px-4 py-2 border border-green-500 text-orange-500 rounded-md hover:bg-orange-500 hover:text-white" onClick={() => setBoxColor("orange")}>Orange</button>
-        <button className="px-4 py-2 border border-green-500 text-purple-500 rounded-md hover:bg-purple-500 hover:text-white" onClick={() => setBoxColor("purple")}>Purple</button>
+export default function Color() {
+  const [boxColor, setBoxColor] = useState("white");
+
+  return (
+    <>
+      <div style={{ padding: "20px" }}>
+
+        <div style={{ marginBottom: "20px", display: "flex", gap: "10px" }}>
+          <button className="px-4 py-2 border border-green-500 text-red-500 rounded-md hover:bg-red-500 hover:text-white" onClick={() => setBoxColor("red")}>Red</button>
+          <button className="px-4 py-2 border border-green-500 text-blue-500 rounded-md hover:bg-blue-500 hover:text-white" onClick={() => setBoxColor("blue")}>Blue</button>
+          <button className="px-4 py-2 border border-green-500 text-green-500 rounded-md hover:bg-green-500 hover:text-white" onClick={() => setBoxColor("green")}>Green</button>
+          <button className="px-4 py-2 border border-green-500 text-orange-500 rounded-md hover:bg-orange-500 hover:text-white" onClick={() => setBoxColor("orange")}>Orange</button>
+          <button className="px-4 py-2 border border-green-500 text-purple-500 rounded-md hover:bg-purple-500 hover:text-white" onClick={() => setBoxColor("purple")}>Purple</button>
+        </div>
+
+
+
+
+        <div className="w-60 h-60 bg-white/20 backdrop-blur-lg border border-white/30 rounded-xl p-6"
+          style={{
+            backgroundColor: boxColor,
+
+            boxShadow: `10px 10px 25px ${boxColor}`,
+            transition: "all 0.3s ease",
+          }}
+
+
+
+        ></div>
       </div>
-
-      
-
-      
-      <div className="w-60 h-60 bg-white/20 backdrop-blur-lg border border-white/30 rounded-xl p-6 shadow-lg"
-        style={{  backgroundColor: boxColor, }}
-       
-        
-        
-      ></div>
-    </div>
-        </>
-    )
+    </>
+  )
 }
